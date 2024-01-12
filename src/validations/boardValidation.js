dc2781481042969d7cdb7a3d507381797122900f
Cuong Nguyen <galvinnguyen1999@gmail.com>
2024-01-12 10:43:09 +0700

    Aggregate

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,11 +1,19 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
+import { BOARD_TYPES } from '~/utils/constants'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
-    title: Joi.string().required().min(3).max(50).trim().strict(),
-    description: Joi.string().required().min(3).max(256).trim().strict()
+    title: Joi.string().required().min(3).max(50).trim().strict().message({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be an empty string',
+      'string.min': 'Title length must be at least 3 characters long',
+      'string.max': 'Title length must be less than or equal to 5 characters long',
+      'string.trim': 'Title must not have leading of trailing whitespace'
+    }),
+    description: Joi.string().required().min(3).max(256).trim().strict(),
+    type: Joi.string().valid(BOARD_TYPES.PRIVATE, BOARD_TYPES.PUBLIC).required()
   })
 
   try {
