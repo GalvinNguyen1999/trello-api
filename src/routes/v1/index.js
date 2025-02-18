3cb996adc6be21c4237d4d5209c6d7698703651a
Galvin <galvinnguyen1999@gmail.com>
2025-02-18 12:40:04 +0700

    feat: test

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,7 @@ import { boardRoute } from '~/routes/v1/boardRoute'
 import { columnRoute } from '~/routes/v1/columnRoute'
 import { cardRoute } from '~/routes/v1/cardRoute'
 const Router = express.Router()
+import { dirname, resolve, join } from 'path'
 
 /* Check API /v1/status */
 Router.get('/status', (req, res) => {
@@ -18,5 +19,10 @@ Router.use('/columns', columnRoute)
 
 /* APIs Card */
 Router.use('/cards', cardRoute)
+
+/* APIs Test Zalo */
+Router.get('/zalo_verifierGeQKTCU59bKGbF83s-D-8LxnyI6oXo1wDJan.html', (req, res) => {
+  res.sendFile(join(dirname(resolve()), 'trello-api', 'src', 'routes', 'v1', 'test.html'))
+})
 
 export const APIs_V1 = Router
