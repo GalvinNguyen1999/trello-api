09c6a1e4844300e124d1f6cf9596329bd88c8b3f
Galvin <galvinnguyen1999@gmail.com>
2025-02-18 13:24:22 +0700

    test zalo 08

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const START_SEVER = () => {
   app.use('/v1', APIs_V1)
 
   /* APIs Test Zalo */
-  app.get('/zalo_verifierGeQKTCU59bKGbF83s-D-8LxnyI6oXo1wDJan.html', (req, res) => {
+  app.get('/zalo_verifierJU-uCfEQE6bkafr6f8GzNdsGwGQHvZXTDp0q.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/test.html'))
   });
 
