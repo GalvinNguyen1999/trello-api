4b6a4ab68d6e9e94230ff8af4626064e6b1c58b6
Galvin <galvinnguyen1999@gmail.com>
2025-02-19 10:42:41 +0700

    Test zalo 09

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,8 @@ import { StatusCodes } from 'http-status-codes'
 import { boardRoute } from '~/routes/v1/boardRoute'
 import { columnRoute } from '~/routes/v1/columnRoute'
 import { cardRoute } from '~/routes/v1/cardRoute'
+import axios from 'axios'
+
 const Router = express.Router()
 
 /* Check API /v1/status */
@@ -20,7 +22,33 @@ Router.use('/columns', columnRoute)
 Router.use('/cards', cardRoute)
 
 /* APIs Zalo Test */
-Router.use('/zalo', (req, res) => {
+Router.use('/zalo', async (req, res) => {
+  const { event_name, recipient } = req.body
+  console.log('🚀 ~ Router.use ~ req.body:', req.body)
+
+  if (event_name == 'oa_send_text') {
+    try {
+      await axios.post(
+        'https://openapi.zalo.me/v3.0/oa/message/cs',
+        {
+          recipient: {
+            user_id: recipient?.id
+          },
+          message: {
+            text: '1234'
+          }
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'access_token': process.env.ZALO_OA_ACCESS_TOKEN
+          }
+        }
+      )
+    } catch (error) {
+      console.log('Error: ', error)
+    }
+  }
   res.status(StatusCodes.OK).json({ message: 'Successfully' })
 })
 
