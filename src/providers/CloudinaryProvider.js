39ec7f1bb5c38e24a0e406bd85e3f4bb1384d626
Galvin <galvinnguyen123@gmail.com>
2026-04-15 00:12:15 +0700

    lesson 11: upload file to cloudinary

---
+++ src/providers/CloudinaryProvider.js
@@ -0,0 +1,31 @@
+import cloudinary from 'cloudinary'
+import streamifier from 'streamifier'
+import { env } from '~/config/environment'
+
+const cloudinaryV2 = cloudinary.v2
+cloudinaryV2.config({
+  cloud_name: env.CLOUDINARY_CLOUD_NAME,
+  api_key: env.CLOUDINARY_API_KEY,
+  api_secret: env.CLOUDINARY_API_SECRET
+})
+
+// Hàm upload file lên cloudinary
+const streamUpload = (fileBuffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinaryV2
+      .uploader
+      .upload_stream(
+        { folder: 'trello' },
+        (error, result) => {
+          if (error) reject(error)
+          else resolve(result)
+        }
+      )
+
+    streamifier.createReadStream(fileBuffer).pipe(stream)
+  })
+}
+
+export const CloudinaryProvider = {
+  streamUpload
+}
